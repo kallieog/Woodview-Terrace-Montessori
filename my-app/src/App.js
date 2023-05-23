9bfe9e05c64e60ce41ff11bb6e3914392b7ddf30
Claholla <claholla@gmail.com>
2023-05-23 17:29:08 -0500

    added navbar template and imported react-bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
