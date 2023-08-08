745eeded9ac2734ed156dcd3e9f43b9aeb8e1880
Claholla <claholla@gmail.com>
2023-08-08 18:17:38 -0500

    fixed landing carousel image sizing

--- my-app/src/components/Landing/Landing.js
+++ my-app/src/components/Landing/Landing.js
@@ -10,27 +10,37 @@ export default function Landing() {
   return (
     <>
       <Container fluid>
-        <Row>
-          <Col sm={12} md={5}>
+        <Row className="my-4 mx-3">
+          <Col sm={12} md={5} align="center">
             <h2>Our Mission:</h2>
             <br />
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-              nibh molestie, lobortis enim at, rutrum tellus. Maecenas quis
-              dolor velit. Nam convallis felis risus, eget lobortis est rhoncus
-              ut.
+              We are a family-centered and family-owned Montessori school
+              providing quality care and education for children ages 6 weeks
+              through 6 years. We offer a variety of options within each program
+              to suit your family’s needs. Let your child’s lifelong love of
+              learning begin!
             </p>
           </Col>
-          <Col sm={12} md={5}>
+          <Col sm={12} md={5} className="my-3">
             <Carousel controls={false} class="carousel-container">
               <Carousel.Item>
-                <img src="https://media.istockphoto.com/id/1031384160/photo/primary-school-kids-run-holding-hands-in-corridor-close-up.jpg?s=612x612&w=0&k=20&c=a7ROQEvnnf7H8KECAlMl9KZAXn8sc38IknUGm7aimQA=" class="carousel-image" />
+                <img
+                  src="https://media.istockphoto.com/id/1031384160/photo/primary-school-kids-run-holding-hands-in-corridor-close-up.jpg?s=612x612&w=0&k=20&c=a7ROQEvnnf7H8KECAlMl9KZAXn8sc38IknUGm7aimQA="
+                  class="carousel-image"
+                />
               </Carousel.Item>
               <Carousel.Item>
-                <img src="https://media.istockphoto.com/id/1170717275/photo/lets-learn-something-new-today.jpg?s=612x612&w=0&k=20&c=cdAb-boDeHfhz1iS2zs-GN_tb7x8PuCefZ4RlsmZdno=" class="carousel-image" />
+                <img
+                  src="https://media.istockphoto.com/id/1170717275/photo/lets-learn-something-new-today.jpg?s=612x612&w=0&k=20&c=cdAb-boDeHfhz1iS2zs-GN_tb7x8PuCefZ4RlsmZdno="
+                  class="carousel-image"
+                />
               </Carousel.Item>
               <Carousel.Item>
-                <img src="https://thumbs.dreamstime.com/b/happy-children-girls-girlfriend-schoolgirl-student-elementary-school-happy-children-girlfriend-schoolgirl-student-elementary-121447690.jpg" class="carousel-image" />
+                <img
+                  src="https://d2c0db5b8fb27c1c9887-9b32efc83a6b298bb22e7a1df0837426.ssl.cf2.rackcdn.com/14460694-community-of-multi-diverse-lear-612x408.jpeg"
+                  class="carousel-image"
+                />
               </Carousel.Item>
             </Carousel>
           </Col>
