192101154b928069605867fc46d4bc93d5c7f15d
kallieog <111663090+kallieog@users.noreply.github.com>
2023-08-08 20:11:17 -0500

    routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,57 @@
 import logo from "./logo.svg";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing/Landing";
-
+import Testimonials from "./components/Testimonials/Testimonials";
+import Infant from "./components/Programs/Infant/Infant";
+import Toddler from "./components/Programs/Toddler/Toddler";
+import Preschool from "./components/Programs/Preschool/Preschool";
+import Kindergarten from "./components/Programs/Kindergarten/Kindergarten";
+import Newsletters from "./components/ParentInformation/Newsletters/Newsletters";
+import Calendar from "./components/ParentInformation/Calendar/Calendar";
+import SchoolForms from "./components/ParentInformation/SchoolForms/SchoolForms";
+import ReadingMaterial from "./components/ParentInformation/ReadingMaterial/ReadingMaterial";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-        crossorigin="anonymous"
-      />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;400;700&family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet" />
-      
-      <Navbar />
-      <Landing />
-    </div>
+    <>
+      <div className="App">
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossorigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;400;700&family=Roboto:ital,wght@1,100&display=swap"
+          rel="stylesheet"
+        />
+        <Router>
+          <>
+            <Navbar />
+
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/Testimonials" element={<Testimonials />} />
+              <Route path="/Infant" element={<Infant />} />
+              <Route path="/Toddler" element={<Toddler />} />
+              <Route path="/Preschool" element={<Preschool />} />
+              <Route path="/Kindergarten" element={<Kindergarten />} />
+              <Route path="/Newsletters" element={<Newsletters />} />
+              <Route path="/Calendar" element={<Calendar />} />
+              <Route path="/SchoolForms" element={<SchoolForms />} />
+              <Route path="ReadingMaterial" element={<ReadingMaterial />} />
+              <Route path="Contact" element={<Contact />} />
+            </Routes>
+          </>
+        </Router>
+      </div>
+    </>
   );
 }
 
--- my-app/src/components/Navbar/navbar.js
+++ my-app/src/components/Navbar/navbar.js
@@ -28,55 +28,50 @@ export default function Navigation() {
             <Offcanvas.Header closeButton></Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="/">Home</Nav.Link>
-          
-              <NavDropdown title="Philosophy" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Mission statement</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    What is a montessori?
-                  </NavDropdown.Item>
-                 
-                  <NavDropdown.Divider />
-                  
-                  {/* <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item> */}
-                </NavDropdown>
+                <Nav.Link href="/">Home</Nav.Link>
+
                 <NavDropdown title="Programs" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Infant</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Toddler
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
+                  <NavDropdown.Item href="/Infant">Infant</NavDropdown.Item>
+                  <NavDropdown.Item href="/Toddler">Toddler</NavDropdown.Item>
+                  <NavDropdown.Item href="/Preschool">
                     Preschool
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
+                  <NavDropdown.Item href="/Kindergarten">
                     Kindergarten
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  
+
                   {/* <NavDropdown.Item href="#action/3.4">
                     Separated link
                   </NavDropdown.Item> */}
                 </NavDropdown>
                 <NavDropdown title="Parent Information" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Newsletters</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
+                  <NavDropdown.Item href="/Newsletters">
+                    Newsletters
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/Calendar">
                     Calendar
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
+                  <NavDropdown.Item href="/SchoolForms">
                     School forms
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
+                  <NavDropdown.Item href="/ReadingMaterial">
                     Reading material
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  
+
                   {/* <NavDropdown.Item href="#action/3.4">
                     Separated link
                   </NavDropdown.Item> */}
                 </NavDropdown>
-                
+
+                <Nav.Link href="/Testimonials">Menu</Nav.Link>
+
+                <Nav.Link href="/Testimonials">Testimonials</Nav.Link>
+
+                <Nav.Link href="/Testimonials">Contact</Nav.Link>
+
+
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
